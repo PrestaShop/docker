7ef4797ee42099a94e7beccb71125fb0fa84e27e
Thomas Nabord <thomas.nabord@prestashop.com>
2015-08-03 14:45:43 +0200

    Add latest versions of PS 1.4 & 1.5

--- update_tags.sh
+++ update_tags.sh
@@ -8,10 +8,10 @@ docker tag -f quetzacoalt/prestashop:1.6.1.0 quetzacoalt/prestashop:latest
 docker tag -f quetzacoalt/prestashop:1.6.0.14 quetzacoalt/prestashop:1.6.0
 
 # PrestaShop 1.5
-docker tag -f quetzacoalt/prestashop:1.5.6.2 quetzacoalt/prestashop:1.5
+docker tag -f quetzacoalt/prestashop:1.5.6.3 quetzacoalt/prestashop:1.5
 
 #PrestaShop 1.4
-docker tag -f quetzacoalt/prestashop:1.4.11.0 quetzacoalt/prestashop:1.4
+docker tag -f quetzacoalt/prestashop:1.4.11.1 quetzacoalt/prestashop:1.4
 
 docker push quetzacoalt/prestashop:1.4
 docker push quetzacoalt/prestashop:1.5
