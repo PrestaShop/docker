665082254afa0796474271f3f5045f732574f984
Thomas Nabord <thomas.nabord@prestashop.com>
2017-08-01 16:35:18 +0100

    Generate all images

--- images/1.4.0.6/config_files/ps-extractor.sh
+++ images/1.4.0.6/config_files/ps-extractor.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 folder=$1
-destination="/var/www/html"
 
-if [[ -n "$folder" ]]; then
-    if [ -d $folder/prestashop ]; then
-        cp -n -R $folder/prestashop/* $destination
-    else
-        unzip -n -q $folder/prestashop.zip -d $destination
+if [[ -n "$folder" ]]; then  
+
+    # dwl version contains zip file with tree structure (1.7)
+    if [ ! -d $folder/prestashop ]; then
+        unzip -n -q $folder/prestashop.zip -d $folder/prestashop
+	rm -rf $folder/prestashop.zip	
     fi
+
+    # prepair tree structure for volumes
+    mv $folder/prestashop/themes $folder/prestashop/modules $folder/prestashop/override $folder/
+
+    # build relative symlinks for volumes
+    ln -s ../themes $folder/prestashop/themes
+    ln -s ../modules $folder/prestashop/modules
+    ln -s ../override $folder/prestashop/override
 else
     echo "Missing folder to move"
 fi
