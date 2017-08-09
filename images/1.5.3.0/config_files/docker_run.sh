2d1739096d0d96705cb84b40e2d02a8aeb86573b
Thomas Nabord <thomas.nabord@prestashop.com>
2017-08-09 18:27:49 +0200

    Generate images

--- images/1.5.3.0/config_files/docker_run.sh
+++ images/1.5.3.0/config_files/docker_run.sh
@@ -66,7 +66,7 @@ if [ ! -f ./config/settings.inc.php  ]; then
 		fi
 
 		php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
-			--db_password=$DB_PASSWD --firstname="John" --lastname="Doe" \
+			--db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
 			--password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
 			--newsletter=0 --send_email=0
 	fi
