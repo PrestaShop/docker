efcdb6deda3c93daf02767b33898ca6bddb479a3
MathiasReker <mathias@reker.dk>
2018-12-27 20:38:45 +0100

    Doctrine separation

--- base/images/7.2-apache/config_files/docker_updt_ps_domains.php
+++ base/images/7.2-apache/config_files/docker_updt_ps_domains.php
@@ -20,7 +20,9 @@
  *
  *  @author    PrestaShop SA <contact@prestashop.com>
  *  @copyright 2007-2016 PrestaShop SA
+ *
  *  @version  Release: $Revision: 14390 $
+ *
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
