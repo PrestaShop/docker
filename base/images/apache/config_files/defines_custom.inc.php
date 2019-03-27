d4fcfd3e59a963c2831d7091d49d93a2b291e27b
Thomas Nabord <thomas.nabord@prestashop.com>
2019-03-27 11:13:30 +0000

    Generate base images

--- base/images/apache/config_files/defines_custom.inc.php
+++ base/images/apache/config_files/defines_custom.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2018 PrestaShop
+ * 2007-2019 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
  *
@@ -16,21 +16,20 @@
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
+ * needs please refer to https://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2018 PrestaShop SA
+ * @copyright 2007-2019 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-
-if (getenv('PS_DEV_MODE')) {
-    define('_PS_MODE_DEV_', getenv('PS_DEV_MODE'));
+if ((bool) getenv('PS_DEV_MODE')) {
+    define('_PS_MODE_DEV_', (bool) getenv('PS_DEV_MODE'));
 }
-if (getenv('PS_DEMO_MODE')) {
-    define('_PS_MODE_DEMO_', getenv('PS_DEMO_MODE'));
+if ((bool) getenv('PS_DEMO_MODE')) {
+    define('_PS_MODE_DEMO_', (bool) getenv('PS_DEMO_MODE'));
 }
 // FYI: Defining env var _PS_HOST_MODE_ as true also works natively
-if (getenv('PS_HOST_MODE')) {
-    define('_PS_HOST_MODE_', getenv('PS_HOST_MODE'));
+if ((bool) getenv('PS_HOST_MODE')) {
+    define('_PS_HOST_MODE_', (bool) getenv('PS_HOST_MODE'));
 }
