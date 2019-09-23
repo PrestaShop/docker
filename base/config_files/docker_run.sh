f1d40abed45eae6538b98ee54eb5c42da89bed0a
Claude-Arnaud Perrot <claude-arnaud.perrot@prestashop.com>
2019-09-23 15:17:52 +0200

    Fixing variable name typo

--- base/config_files/docker_run.sh
+++ base/config_files/docker_run.sh
@@ -59,12 +59,12 @@ if [ ! -f ./config/settings.inc.php  ]; then
 			if [ $DB_PASSWD = "" ]; then
                         	echo "\n* Dropping existing database $DB_NAME..."
 				mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "drop database if exists $DB_NAME;"
-				echo "\n* Creating database $DN_NAME..."
+				echo "\n* Creating database $DB_NAME..."
 				mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER create $DB_NAME -p$DB_PASSWD --force;
 			else
 				echo "\n* Dropping existing database $DB_NAME..."
 				mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "drop database if exists $DB_NAME;"
-				echo "\n* Creating database $DN_NAME..."
+				echo "\n* Creating database $DB_NAME..."
 				mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD create $DB_NAME --force;
 			fi
 		fi
