b527828dbae7332736fafd651e3e8750352ac0fb
Thomas Nabord <thomas.nabord@prestashop.com>
2019-11-04 11:53:18 +0000

    Update base images

--- base/images/7.0-fpm/config_files/defines_custom.inc.php
+++ base/images/7.0-fpm/config_files/defines_custom.inc.php
@@ -26,9 +26,6 @@
 if ((bool) getenv('PS_DEV_MODE')) {
     define('_PS_MODE_DEV_', (bool) getenv('PS_DEV_MODE'));
 }
-if ((bool) getenv('PS_DEMO_MODE')) {
-    define('_PS_MODE_DEMO_', (bool) getenv('PS_DEMO_MODE'));
-}
 // FYI: Defining env var _PS_HOST_MODE_ as true also works natively
 if ((bool) getenv('PS_HOST_MODE')) {
     define('_PS_HOST_MODE_', (bool) getenv('PS_HOST_MODE'));
