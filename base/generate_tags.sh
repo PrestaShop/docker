2ea1e311f8d0b7507735dbb7cc5b30e41ab5c684
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2020-03-04 17:58:55 +0100

    Refactor bash script and remove /usr/include for gd library

--- base/generate_tags.sh
+++ base/generate_tags.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-pushd $(cd "$( dirname "$0" )" && pwd)
+cd $(cd "$( dirname "$0" )" && pwd)
+
+if [ -z "$1" ]; then
+    PS_VERSIONS_FILE="tags.txt";
+else
+    PS_VERSIONS_FILE="$1";
+fi
 
 generate_image()
 {
     echo "Generate Dockerfile for PHP $version"
 
-    folder="$version";
-    exec="apache2-foreground";
+    local version=$1
+    local folder="${version}";
+    local exec="apache2-foreground";
+
     if [[ $version = *"fpm"* ]]; then
       exec='php-fpm'
     fi
@@ -15,26 +23,19 @@ generate_image()
 
     mkdir -p images/$folder/
 
-    sed  '
-            s/{PHP_TAG}/'"$version"'/
+    sed '
+            s/{PHP_TAG}/'"${version}"'/
         ' Dockerfile.model > images/$folder/Dockerfile
 
     cp -R config_files/ images/$folder/
-    sed  '
-            s/{PHP_CMD}/'"$exec"'/
+    sed '
+            s/{PHP_CMD}/'"${exec}"'/
         ' config_files/docker_run.sh > images/$folder/config_files/docker_run.sh
 }
 
-if [ -z "$1" ]; then
-    ps_versions_file="tags.txt";
-else
-    ps_versions_file="$1";
-fi
 
 # Generate base images for PHP tags
-echo "Reading tags in $ps_versions_file ..."
+echo "Reading tags in ${PS_VERSIONS_FILE} ..."
 while read version; do
-    generate_image
-done <$ps_versions_file
-
-popd
+    generate_image $version
+done < $PS_VERSIONS_FILE
