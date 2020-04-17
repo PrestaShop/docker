d308697773fdaf81e25dfe228bf93faf811271de
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2020-04-17 16:59:52 +0200

    Release 1.7.6.5, fix script where bash function can't take 2 arrays as arguments

--- generate_all.sh
+++ generate_all.sh
@@ -40,17 +40,6 @@ generate_image()
     fi
 }
 
-parse_versions()
-{
-    # Generate images for each major VERSION of PrestaShop on different PHP environment
-    for php_version in $1; do
-        for major_version in $2; do
-            version=$(grep "." "versions${major_version}.txt" | tail -1)
-            generate_image $php_version $version
-        done
-    done
-}
-
 # Generate images for all PrestaShop versions from 1.4 on PHP 5.6
 echo "Reading versions in $PS_VERSIONS_FILE ..."
 while read version; do
@@ -58,11 +47,24 @@ while read version; do
 done < $PS_VERSIONS_FILE
 
 # Specific to old PrestaShop versions
+echo "Reading versions in versions15.txt and versions16.txt ..."
 PHP_VERSION=("5.6-apache" "7.0-apache" "7.1-apache" "5.6-fpm" "7.0-fpm" "7.1-fpm")
 MAJOR_VERSION=("15" "16")
-parse_versions $PHP_VERSION $MAJOR_VERSION
+# Generate images for each major VERSION of PrestaShop on different PHP environment
+for php_version in ${PHP_VERSION[@]}; do
+    for major_version in ${MAJOR_VERSION[@]}; do
+        version=$(grep "." "versions${major_version}.txt" | tail -1)
+        generate_image $php_version $version
+    done
+done
 
 # Used for 1.7 and nightly versions
+echo "Reading versions in versionsnightly.txt and versions17.txt ..."
 PHP_VERSION=("7.1-apache" "7.2-apache" "7.3-apache" "7.1-fpm" "7.2-fpm" "7.3-fpm")
 MAJOR_VERSION=("17" "nightly")
-parse_versions $PHP_VERSION $MAJOR_VERSION
+for php_version in ${PHP_VERSION[@]}; do
+    for major_version in ${MAJOR_VERSION[@]}; do
+        version=$(grep "." "versions${major_version}.txt" | tail -1)
+        generate_image $php_version $version
+    done
+done
