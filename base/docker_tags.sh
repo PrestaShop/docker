abbedc315aaf0fbd4b8dd6970fd5635678b73b9b
Progi1984 <franck.lefevre@prestashop.com>
2023-06-02 12:20:36 +0200

    Add script for build, tag & push base images

---
+++ base/docker_tags.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd $(cd "$( dirname "$0" )" && pwd)
+
+if [ -z "$1" ] || [ "$1" == "-f" ]; then
+    PS_VERSIONS_FILE="tags.txt";
+else
+    PS_VERSIONS_FILE="$1";
+fi
+
+while getopts ":p" option; do
+   case $option in
+      f)
+         PUSH=true
+         ;;
+   esac
+done
+
+docker_image()
+{
+    echo "Docker build & tag : prestashop/base:$version"
+    
+    id=$(echo $(docker build --quiet=true images/${version} 2>/dev/null) | awk '{print $NF}')
+    echo $id;
+    docker tag $id prestashop/base:${version}
+
+
+    if [ -z "$PUSH" ]; then
+        # Do not push
+        return
+    fi
+    echo "Docker Push : prestashop/base:$version"
+
+    docker push prestashop/base:${version}
+}
+
+
+# Generate base images for PHP tags
+echo "Reading tags in ${PS_VERSIONS_FILE} ..."
+while read version; do
+    docker_image $version
+done < $PS_VERSIONS_FILE
