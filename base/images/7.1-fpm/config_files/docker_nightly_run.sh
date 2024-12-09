8220cfb2ed5bb0a122a2ada06d7aea3a485f81c9
Progi1984 <franck.lefevre@prestashop.com>
2024-12-09 14:30:47 +0100

    Nightly : Fixed grep with env variable

--- base/images/7.1-fpm/config_files/docker_nightly_run.sh
+++ base/images/7.1-fpm/config_files/docker_nightly_run.sh
@@ -2,7 +2,7 @@
 
 # Download nightly build
 if [ ! -d /tmp/data-ps ]; then
-    gsutil cp `gsutil ls gs://prestashop-core-nightly/ | grep -E '$PS_VERSION.+\.zip$' | tail -1` /tmp/prestashop.zip
+    gsutil cp `gsutil ls gs://prestashop-core-nightly/ | grep -E "$PS_VERSION.+\.zip$" | tail -1` /tmp/prestashop.zip
 
     mkdir -p /tmp/data-ps
     unzip -q /tmp/prestashop.zip -d /tmp/data-ps/
